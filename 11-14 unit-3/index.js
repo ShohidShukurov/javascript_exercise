d53d9660dfddf6711e12f0c71715dcc8e2337324
Shohid <shukurovshohid7@gmail.com>
2022-11-17 18:56:11 +0500

    If else, Ternarny operator, Nullish

---
+++ 11-14 unit-3/index.js
@@ -0,0 +1,48 @@
+// if ("0") {
+//   console.log('hello');
+//  } else {console.log('goodbye');
+// }
+
+// ================>
+//   let jsname = prompt()
+//    if (jsname === "EcmaScript")
+//    console.log('right');
+//    else {console.log('You don`t know? "ECMAScript"'); }
+
+//==========>
+
+// let Input = "4";
+
+// if (+Input > 0) {
+//   console.log(1);
+// } else if (+Input < 0) {
+//   console.log(-1);
+// } else if (+Input === 0) {
+//   console.log(0);
+// } else {
+//   console.log("Input only number");
+// }
+
+// let a = 6;
+// let b = 4;
+
+// let result;
+
+// if (a + b < 4) result = "Below";
+// else result = "Over";
+
+// console.log(result); jabvob over chunki a va b ning yigindisi 4 katta agar a va b ning qiymati 4 dan kichik bo`lasa below chiqadi.
+
+//====================>
+
+// let login = 'Employee';
+
+// let message;
+
+// login == 'Employee'
+// ? (message = 'Hello')
+// : (login == 'director')
+// ? (message == 'Greetings')
+// : (login == '' )
+// ? (message == 'no login')
+// : (message == '')
