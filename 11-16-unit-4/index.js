46a423267e5844764d34edd649b4660d4f7bac4e
Shohid <shukurovshohid7@gmail.com>
2022-11-17 18:58:03 +0500

    Switch statement, For Loops

---
+++ 11-16-unit-4/index.js
@@ -0,0 +1,44 @@
+// let browzer = "edge";
+
+// switch (browzer) {
+//   case "edge":
+//     console.log("You`ve got tje edge");
+//     break;
+// }
+// case 'chrome'
+// case 'firefox'
+// case 'safari'
+// case 'opera'
+// console.log('okay we support these browser too');
+// break;
+
+// default:
+//     console.log('we hope that this page looks ok!');
+
+// izoh; bu yerda casega qiymatlar berilgan tepadagi 1-case tog'ri kelmasa avtomatik keyingisiga o`tadi pasdagi caselardan ham hech qaysi chiqmasa defultdagi qiymati chiqadi
+
+//================>
+
+// let a = 2;
+// if (a == 0) {
+//   console.log(0);
+// }
+// if (a == 1) {
+//   console.log(1);
+// }
+// if (a == 2||  a == 3) {
+//   console.log("2,3");
+// }
+
+// javob '2,3' sabab tepada 2 ni qanoatlantiradigan qiymat yuq pasta or bor or 2 va 3 qiymat bor biz bilamski or ikta qiymatdan qaysi biri to`g`ri bulsa bo`ldi  olib beradi shuning uchun '2,3'
+
+// for (let i = 2; i <= 9; i++) {
+//   console.log("===========");
+//   console.log(`${i} Karra`);
+//   console.log("===========");
+//   for (let j = 1; j <= 10; j++) {
+//     console.log(`${i} * ${j} = ${i * j}`);
+//   }
+// }
+
+
