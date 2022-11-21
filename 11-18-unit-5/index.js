6c8ab05ea47e27b04d012c653794690243ca2d1f
Shohid <shukurovshohid7@gmail.com>
2022-11-21 20:09:07 +0500

    While,Function

---
+++ 11-18-unit-5/index.js
@@ -0,0 +1,58 @@
+//exercise=1
+// let i = 0;
+
+// while (i < 3) {
+//   console.log(i);
+//   i++;
+// }
+
+// exercise-2
+// function checkage(age) {
+//   if (age > 18) {
+//     return true;
+//   } else {
+//     console.log("gsdfgsdfg");
+//   }
+// }
+
+// console.log(checkage(19));
+
+// exercise 3
+
+// const checkAge = (age) => {
+//   return age > 18 ? true : "Did parents allow you";
+// };
+// console.log(checkAge(19));
+
+// const checkAge = (age) => {
+//   return age > 18 || "Did parents allow you";
+// };
+// console.log(checkAge(19));
+
+// exercise 4
+
+// const min = (a, b) => {
+//   return a > b ? a : b;
+// };
+
+// console.log(min(3, 2));
+
+// exercise 5
+
+// const pow = (x, n) => {
+//   return x ** n;
+// };
+// console.log(pow(2, 3));
+
+// exercise 6
+
+// ask = (question, yes, no) => {
+//   if (confirm(question)) yes();
+//   else no();
+// };
+
+// ask(
+//   "do you agree",
+//   () => alert("you agreed"),
+//   () => alert("you canceled the execution")
+// );
