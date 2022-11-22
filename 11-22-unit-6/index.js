361bf0d9e282821d13d66dddc0107b278772e15e
Shohid <shukurovshohid7@gmail.com>
2022-11-22 22:46:24 +0500

    11-22-unit-6

---
+++ 11-22-unit-6/index.js
@@ -0,0 +1,119 @@
+// tsak 1
+// const user = {
+//   name: "John",
+//   surname: "Smith",
+// };
+
+// user.name = "Pete";
+// delete user.name;
+// console.log(user);
+
+// // task 3
+// let user = {};
+// const isEmpty = (obj) => {
+//   for (let key in obj) {
+//     return false;
+//   }
+//   return true;
+// };
+// console.log(isEmpty(user));
+
+// let data = {
+//   width: 200,
+//   height: 100,
+//   title: "NaN",
+// };
+
+// data.age = 15;
+// console.log(data);
+
+// const check = (obj) => {
+//   let newobj = {};
+//   for (let key in obj) {
+//     if (typeof obj[key] === "number") newobj[key] = obj[key] * 2;
+//     else newobj[key] = obj[key];
+//   }
+//   console.log(newobj);
+// };
+// check(data);
+
+// task 4
+
+// let obj = {
+//   id: 1,
+//   name: "WebBrain",
+// };
+
+// const chesk = (obj) => {
+//   for (let key in obj) {
+//     obj[key];
+//     console.log(key);
+//   }
+// };
+// chesk(obj);
+// task 5
+
+// let obj = {
+//   id: 1,
+//   name: "WebBrain",
+// };
+
+// const chesk = (obj) => {
+//   for (let key in obj) {
+//     if (typeof obj[key] === "number") console.log(key);
+//   }
+// };
+// chesk(obj);
+
+// task 6
+
+// let obj = {
+//   id: 1,
+//   name: "WebBrain",
+//   offline: true,
+//   online: true,
+//   individual: false,
+// };
+
+// const chesk = (obj) => {
+//   for (let key in obj) {
+//     if (typeof obj[key] === "boolean") console.log(key, obj[key]);
+//   }
+// };
+// chesk(obj);
+
+// task 7
+// let obj = {
+//   id: 1,
+//   name: "WebBrain",
+//   offline: true,
+//   online: true,
+//   individual: false,
+// };
+
+// const chesk =(obj)=>{
+//     for(let key in obj){
+//         if(typeof obj[key]==="string"){
+//             console.log(key,obj[key]);
+//         }
+//     }
+// }
+// chesk(obj)
+
+// task 8
+// let obj = {
+//   id: 1,
+//   name: "atirgul",
+//   color: "qizil",
+//   count: 10,
+//   price: "10$",
+// };
+
+// const chesk = (obj) => {
+//   for (let key in obj) {
+//     if (typeof obj[key ]=== "string") {
+//       console.log(obj[key]);
+//     }
+//   }
+// };
+// chesk(obj);
